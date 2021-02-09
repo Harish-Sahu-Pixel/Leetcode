b761372e342c9c395fe38d45b1846bda938ebe00
Harish Kumar Sahu <harishkumarsahu0806@gmail.com>
2021-02-09 09:58:12 +0530

    Update 8_Feb_2021_Problem.py

--- feb leetcoding challenge/8_Feb_2021_Problem.py
+++ feb leetcoding challenge/8_Feb_2021_Problem.py
@@ -31,25 +31,29 @@ class PeekingIterator:
         Initialize your data structure here.
         :type iterator: Iterator
         """
-        
+        self.iter = iterator
+        self.temp = self.iter.next() if self.iter.hasNext() else None
 
     def peek(self):
         """
         Returns the next element in the iteration without advancing the iterator.
         :rtype: int
         """
-        
+        return self.temp
 
     def next(self):
         """
         :rtype: int
         """
-        
+        ans = self.temp
+        self.temp = self.iter.next() if self.iter.hasNext() else None
+        return ans
 
     def hasNext(self):
         """
         :rtype: bool
         """
+        return self.temp is not None
         
 
 # Your PeekingIterator object will be instantiated and called as such:
